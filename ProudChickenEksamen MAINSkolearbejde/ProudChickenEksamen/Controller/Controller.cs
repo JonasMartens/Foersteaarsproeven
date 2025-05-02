295c256132f3c756b7ed3603509d2fbd9aea059e
ChristianSAndersen <chan8@student.eadania.dk>
2025-05-02 11:33:14 +0200

    Efter Jakups indblanding

---
+++ ProudChickenEksamen MAINSkolearbejde/ProudChickenEksamen/Controller/Controller.cs
@@ -0,0 +1,36 @@
+﻿using ProudChickenEksamen.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProudChickenEksamen.Controller
+{
+    class Controller
+    {
+        GUI Gui = new GUI();
+        public void Run()
+        {
+            while (true)
+            {
+                int choice = Gui.MainMenuMetode();
+
+                switch (choice)
+                {
+                    case 1:
+                        Gui.VælgSMS();
+                        break;
+
+                    case 2:
+                        Gui.VælgEMail();
+                        break;
+                    default:
+                        Console.WriteLine("Ugyldigt valg!");
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ ProudChickenEksamen MAINSkolearbejde/ProudChickenEksamen/Model/Entities/EMail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProudChickenEksamen.Model.Entities
+{
+    class EMail
+    {
+        public string ID { get; set; }
+        public string Dato { get; set; }
+        public string EmailStandardBesked { get; set; }
+        public string Custom { get; set; }
+    }
+}
