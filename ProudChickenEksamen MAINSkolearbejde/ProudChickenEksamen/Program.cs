b6ad5f8078a450ae91a2ca7325949a92a42f3574
ChristianSAndersen <chan8@student.eadania.dk>
2025-05-26 11:46:47 +0200

    Delete ProudChickenEksamen MAINSkolearbejde directory
    pga de ekstra classer, som dukker op, som vi ikke bruger

--- ProudChickenEksamen MAINSkolearbejde/ProudChickenEksamen/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using ProudChickenEksamen.Controller;
-using ProudChickenEksamen.Data;
-
-Console.WriteLine();
-IRepository repository = new SQLRepository();
-Controller controller = new Controller(repository);
-controller.Run();
